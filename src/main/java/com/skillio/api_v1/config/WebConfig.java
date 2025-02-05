e65353a2e348714f4cbccd87eefa63f47982af51
Pablo <pablo.and.ramirez@outlook.com>
2025-02-05 11:22:13 -0300

    web configuration file and cors registry added

---
+++ src/main/java/com/skillio/api_v1/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.skillio.api_v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOriginPatterns("https://skillio.netlify.app/") // Permitir solicitudes desde ddterminado origen
+                .allowedOriginPatterns("http://test-skillio.netlify.app/") // Permitir solicitudes desde ddterminado origen
+                .allowedOriginPatterns("http://localhost:4200/") // Permitir solicitudes desde ddterminado origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permitir estos métodos HTTP
+                .allowedHeaders("Content-Type") // Permitir estos encabezados en las solicitudes
+                .allowCredentials(true) // Permitir el envío de credenciales (cookies, tokens de autenticación, etc.)
+                .maxAge(3600); // Tiempo máximo que los resultados preflight serán cacheados
+    }
+
+}
